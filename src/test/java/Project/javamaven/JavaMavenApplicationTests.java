1cfae68ad2b4f6f06f0bae22d1051ea3b48b2184
student <komalrast2411@gmail.com>
2021-04-29 12:41:20 +0000

    creating java-maven app

---
+++ src/test/java/Project/javamaven/JavaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.javamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
